62acb12cc47883917a9c61cc40bb6c29542eda24
BryanCruz <bryan.bialokur@gmail.com>
2024-02-26 00:29:41 -0300

    refact: move common code to a common fn

--- benches/min_spanning_tree.rs
+++ benches/min_spanning_tree.rs
@@ -9,14 +9,18 @@ use test::Bencher;
 mod common;
 use common::*;
 
-use petgraph::algo::min_spanning_tree;
+use petgraph::{
+    algo::min_spanning_tree,
+    visit::{Data, IntoEdgeReferences, IntoNodeReferences, NodeIndexable},
+    Graph,
+};
 
 #[bench]
 fn min_spanning_tree_praust_undir_bench(bench: &mut Bencher) {
     let a = ungraph().praust_a();
     let b = ungraph().praust_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
 }
 
 #[bench]
@@ -24,7 +28,7 @@ fn min_spanning_tree_praust_dir_bench(bench: &mut Bencher) {
     let a = digraph().praust_a();
     let b = digraph().praust_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
 }
 
 #[bench]
@@ -32,7 +36,7 @@ fn min_spanning_tree_full_undir_bench(bench: &mut Bencher) {
     let a = ungraph().full_a();
     let b = ungraph().full_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
 }
 
 #[bench]
@@ -40,7 +44,7 @@ fn min_spanning_tree_full_dir_bench(bench: &mut Bencher) {
     let a = digraph().full_a();
     let b = digraph().full_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
 }
 
 #[bench]
@@ -48,13 +52,22 @@ fn min_spanning_tree_petersen_undir_bench(bench: &mut Bencher) {
     let a = ungraph().petersen_a();
     let b = ungraph().petersen_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
 }
 
 #[bench]
 fn min_spanning_tree_petersen_dir_bench(bench: &mut Bencher) {
-    let a = digraph().petersen_a();
-    let b = digraph().petersen_b();
+    let a: Graph<(), ()> = digraph().petersen_a();
+    let b: Graph<(), ()> = digraph().petersen_b();
 
-    bench.iter(|| (min_spanning_tree(&a), min_spanning_tree(&b)));
+    bench_min_spanning_tree(bench, &a, &b)
+}
+
+fn bench_min_spanning_tree<G>(bench: &mut Bencher, a: G, b: G)
+where
+    G: Data + IntoNodeReferences + IntoEdgeReferences + NodeIndexable,
+    G::NodeWeight: Clone,
+    G::EdgeWeight: Clone + PartialOrd,
+{
+    bench.iter(|| (min_spanning_tree(a), min_spanning_tree(b)));
 }
