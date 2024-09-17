fbd9c4500d5ca8e1f6a47ee1cd692c5ac90b49dc
BryanCruz <bryan.bialokur@gmail.com>
2024-09-17 09:52:42 -0300

    more tests

---
+++ benches/bridges.rs
@@ -0,0 +1,71 @@
+#![feature(test)]
+
+extern crate petgraph;
+extern crate test;
+
+use std::{fs::File, hash::Hash, io::Read};
+
+use test::Bencher;
+
+#[allow(dead_code)]
+mod common;
+use common::ungraph;
+
+use petgraph::{
+    algo::bridges::BridgesSearch,
+    graph6::FromGraph6,
+    visit::{IntoNeighbors, IntoNodeIdentifiers},
+    Graph, Undirected,
+};
+
+#[bench]
+fn bridges_search_praust(bench: &mut Bencher) {
+    let a = ungraph().praust_a();
+    let b = ungraph().praust_b();
+
+    bench.iter(|| (iterate_bridges_search(&a), iterate_bridges_search(&b)));
+}
+
+#[bench]
+fn bridges_search_full(bench: &mut Bencher) {
+    let a = ungraph().full_a();
+    let b = ungraph().full_b();
+
+    bench.iter(|| (iterate_bridges_search(&a), iterate_bridges_search(&b)));
+}
+
+#[bench]
+fn bridges_search_petersen(bench: &mut Bencher) {
+    let a: Graph<(), (), Undirected> = ungraph().petersen_a();
+    let b = ungraph().petersen_b();
+
+    bench.iter(|| (iterate_bridges_search(&a), iterate_bridges_search(&b)));
+}
+
+#[bench]
+fn bridges_search_2000n(bench: &mut Bencher) {
+    let g = graph_from_graph6_file("tests/res/graph_2000n.g6");
+    bench.iter(|| iterate_bridges_search(&g));
+}
+
+fn iterate_bridges_search<G, N>(g: G)
+where
+    N: Hash + Eq + Copy,
+    G: IntoNeighbors<NodeId = N> + IntoNodeIdentifiers,
+{
+    let start = g.node_identifiers().next().unwrap();
+    let mut bridges_search = BridgesSearch::new(start, g);
+
+    while let Some(edge) = bridges_search.next(g) {
+        std::hint::black_box(edge);
+    }
+}
+
+/// Parse a file in graph6 format into an undirected graph
+fn graph_from_graph6_file(path: &str) -> Graph<(), (), Undirected, u32> {
+    let mut f = File::open(path).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("failed to read from file");
+    Graph::from_graph6_string(contents)
+}
--- tests/bridges.rs
+++ tests/bridges.rs
@@ -1,7 +1,7 @@
 use petgraph::{algo::bridges::BridgesSearch, dot::Dot, Graph};
 
 #[test]
-fn bridges_test() {
+fn bridges_test_a() {
     let mut gr = Graph::new_undirected();
     let a = gr.add_node("A");
     let b = gr.add_node("B");
@@ -19,6 +19,44 @@ fn bridges_test() {
 
     let mut bridges_iter = BridgesSearch::new(a, &gr);
     println!("{:#?}", bridges_iter);
+
+    let b_c = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter);
+    assert_eq!(b_c, Some((b, c)));
+
+    let a_b = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter);
+    assert_eq!(a_b, Some((a, b)));
+
+    assert_eq!(bridges_iter.next(&gr), None);
+    assert_eq!(bridges_iter.next(&gr), None);
+}
+
+#[test]
+fn bridges_test_b() {
+    let mut gr = Graph::new_undirected();
+    let a = gr.add_node("A");
+    let b = gr.add_node("B");
+    let c = gr.add_node("C");
+    let d = gr.add_node("D");
+    let e = gr.add_node("E");
+    let f = gr.add_node("F");
+
+    gr.add_edge(a, b, 1.);
+    gr.add_edge(b, c, 2.);
+    gr.add_edge(c, d, 3.);
+    gr.add_edge(c, e, 4.);
+    gr.add_edge(d, e, 5.);
+    gr.add_edge(e, f, 6.);
+
+    println!("{}", Dot::new(&gr));
+
+    let mut bridges_iter = BridgesSearch::new(a, &gr);
+    println!("{:#?}", bridges_iter);
+
+    let e_f = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter);
+    assert_eq!(e_f, Some((e, f)));
 
     let b_c = bridges_iter.next(&gr);
     println!("{:#?}", bridges_iter);
