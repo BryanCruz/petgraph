dc8a7066c91692c418f6539b079ae41550f1fc3b
BryanCruz <bryan.bialokur@gmail.com>
2024-09-27 17:49:54 -0300

    change impl

--- src/algo/bridges.rs
+++ src/algo/bridges.rs
@@ -1,17 +1,16 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, fmt::Debug, hash::Hash};
 
 use super::IntoNeighbors;
 
-#[derive(Debug)]
-pub struct BridgesSearch<N> {
+pub struct BridgesSearch<'a, N> {
     /// The stack of nodes to visit
     pub color: HashMap<N, Color>,
     /// The map of preorder number a node is reached in DFS search
     pub pre: HashMap<N, usize>,
     /// The map of lowest preorder number a node is reachable in DFS search
     pub low: HashMap<N, usize>,
-    pub dfs_stack: Vec<N>,
     pub edges_stack: Vec<(N, N)>,
+    pub neighbours: HashMap<N, Box<dyn Iterator<Item = N> + 'a>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -20,75 +19,62 @@ pub enum Color {
     Black,
 }
 
-impl<N> BridgesSearch<N>
+impl<'a, N> BridgesSearch<'a, N>
 where
-    N: Hash + Eq + Copy,
+    N: Hash + Eq + Copy + Debug,
 {
-    pub fn new<G>(start: N, graph: G) -> Self
-    where
-        G: IntoNeighbors<NodeId = N>,
-    {
-        let mut dfs_stack = Vec::new();
-        dfs_stack.push(start);
-
-        let mut color = HashMap::new();
-        let mut pre = HashMap::new();
-        let mut low = HashMap::new();
+    pub fn new(start: N) -> Self {
         let mut edges_stack = Vec::new();
-
-        while let Some(a) = dfs_stack.pop() {
-            if color.contains_key(&a) {
-                continue;
-            }
-
-            let cnt = pre.len();
-            pre.insert(a, cnt);
-            low.insert(a, cnt);
-            color.insert(a, Color::Gray);
-
-            for b in graph.neighbors(a) {
-                if !color.contains_key(&b) {
-                    dfs_stack.push(b);
-                    edges_stack.push((a, b));
-                }
-            }
-        }
-
+        edges_stack.push((start, start));
         BridgesSearch {
-            color,
-            pre,
-            low,
-            dfs_stack,
+            color: HashMap::new(),
+            pre: HashMap::new(),
+            low: HashMap::new(),
             edges_stack,
+            neighbours: HashMap::new(),
         }
     }
 
     pub fn next<G>(&mut self, graph: G) -> Option<(N, N)>
     where
-        G: IntoNeighbors<NodeId = N>,
+        G: 'a + IntoNeighbors<NodeId = N>,
     {
-        while let Some((parent, a)) = self.edges_stack.pop() {
-            if self.color.get(&a) == Some(&Color::Black) {
-                continue;
+        while !self.edges_stack.is_empty() {
+            let (parent, a) = *self.edges_stack.last().unwrap();
+
+            if self.color.get(&a) == None {
+                let cnt = self.color.len();
+                self.color.insert(a, Color::Gray);
+                self.pre.insert(a, cnt);
+                self.low.insert(a, cnt);
+                self.neighbours.insert(a, Box::new(graph.neighbors(a)));
             }
 
-            self.color.insert(a, Color::Black);
-            for b in graph.neighbors(a) {
-                if b == parent {
-                    continue;
+            if self.color.get(&a) == Some(&Color::Gray) {
+                if let Some(b) = (*self.neighbours.get_mut(&a).unwrap()).next() {
+                    if self.color.get(&b) == None {
+                        self.edges_stack.push((a, b));
+                    } else if b != parent {
+                        let low_a = *self.low.get(&a).unwrap();
+                        let pre_b = *self.pre.get(&b).unwrap();
+                        if low_a > pre_b {
+                            self.low.insert(a, pre_b);
+                        }
+                    }
+                } else {
+                    self.color.insert(a, Color::Black);
                 }
-
+            } else {
+                self.edges_stack.pop();
+                let low_parent = *self.low.get(&parent).unwrap();
                 let low_a = *self.low.get(&a).unwrap();
-                let low_b = *self.low.get(&b).unwrap();
-                if low_a > low_b {
-                    self.low.insert(a, low_b);
+                let pre_a = *self.pre.get(&a).unwrap();
+                if low_a < low_parent {
+                    self.low.insert(parent, low_a);
                 }
-            }
-
-            let low_a = *self.low.get(&a).unwrap();
-            let pre_a = *self.pre.get(&a).unwrap();
-            if low_a == pre_a {
-                return Some((parent, a));
+                if low_a == pre_a && parent != a {
+                    return Some((parent, a));
+                }
             }
         }
 
--- tests/bridges.rs
+++ tests/bridges.rs
@@ -17,15 +17,18 @@ fn bridges_test_a() {
 
     println!("{}", Dot::new(&gr));
 
-    let mut bridges_iter = BridgesSearch::new(a, &gr);
-    println!("{:#?}", bridges_iter);
+    let mut bridges_iter = BridgesSearch::new(a);
+    println!("{:#?}", bridges_iter.pre);
+    println!("{:#?}", bridges_iter.color);
 
     let b_c = bridges_iter.next(&gr);
-    println!("{:#?}", bridges_iter);
+    println!("{:#?}", bridges_iter.pre);
+    println!("{:#?}", bridges_iter.color);
     assert_eq!(b_c, Some((b, c)));
 
     let a_b = bridges_iter.next(&gr);
-    println!("{:#?}", bridges_iter);
+    println!("{:#?}", bridges_iter.pre);
+    println!("{:#?}", bridges_iter.color);
     assert_eq!(a_b, Some((a, b)));
 
     assert_eq!(bridges_iter.next(&gr), None);
@@ -51,19 +54,57 @@ fn bridges_test_b() {
 
     println!("{}", Dot::new(&gr));
 
-    let mut bridges_iter = BridgesSearch::new(a, &gr);
-    println!("{:#?}", bridges_iter);
+    let mut bridges_iter = BridgesSearch::new(a);
+    println!("{:#?}", bridges_iter.pre);
 
     let e_f = bridges_iter.next(&gr);
-    println!("{:#?}", bridges_iter);
+    println!("{:#?}", bridges_iter.pre);
     assert_eq!(e_f, Some((e, f)));
 
     let b_c = bridges_iter.next(&gr);
-    println!("{:#?}", bridges_iter);
+    println!("{:#?}", bridges_iter.pre);
     assert_eq!(b_c, Some((b, c)));
 
     let a_b = bridges_iter.next(&gr);
-    println!("{:#?}", bridges_iter);
+    println!("{:#?}", bridges_iter.pre);
+    assert_eq!(a_b, Some((a, b)));
+
+    assert_eq!(bridges_iter.next(&gr), None);
+    assert_eq!(bridges_iter.next(&gr), None);
+}
+
+#[test]
+fn bridges_test_c_1() {
+    let mut gr = Graph::new_undirected();
+    let a = gr.add_node("A");
+    let b = gr.add_node("B");
+    let c = gr.add_node("C");
+    let d = gr.add_node("D");
+    let e = gr.add_node("E");
+    let f = gr.add_node("F");
+
+    gr.add_edge(a, b, 1.);
+    gr.add_edge(b, c, 2.);
+    gr.add_edge(c, d, 3.);
+    gr.add_edge(c, e, 4.);
+    gr.add_edge(d, e, 5.);
+    gr.add_edge(e, f, 6.);
+
+    println!("{}", Dot::new(&gr));
+
+    let mut bridges_iter = BridgesSearch::new(a);
+    println!("{:#?}", bridges_iter.pre);
+
+    let e_f = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter.pre);
+    assert_eq!(e_f, Some((e, f)));
+
+    let b_c = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter.pre);
+    assert_eq!(b_c, Some((b, c)));
+
+    let a_b = bridges_iter.next(&gr);
+    println!("{:#?}", bridges_iter.pre);
     assert_eq!(a_b, Some((a, b)));
 
     assert_eq!(bridges_iter.next(&gr), None);
