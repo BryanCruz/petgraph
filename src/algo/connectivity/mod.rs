1619b53b48f17fcad02d0e197cc9d057174fd1bc
Bryan Bialokur da Cruz <bryan.bialokur@gmail.com>
2024-10-23 13:49:18 -0300

    Update src/algo/connectivity/mod.rs

--- src/algo/connectivity/mod.rs
+++ src/algo/connectivity/mod.rs
@@ -2,7 +2,7 @@ pub use cut_edges::CutEdgesSearch;
 
 mod cut_edges;
 
-/// Marker type for bridges search.
+/// Marker type used in DFS searches.
 #[derive(Debug, PartialEq)]
 pub enum Color {
     Gray,
